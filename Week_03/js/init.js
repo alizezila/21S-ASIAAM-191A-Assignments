2f80f1ccf93f0ca7b8ec5ed4c44ab31e0ac019ee
alize <81606646+alizezila@users.noreply.github.com>
2021-04-20 15:02:09 -0700

    lab

--- Week_03/js/init.js
+++ Week_03/js/init.js
@@ -1,16 +1,39 @@
+// declare variables
 let zoomLevel = 5;
 const mapCenter = [34.0709,-118.444];
 
+// use the variables
 const map = L.map('map').setView(mapCenter, zoomLevel);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-function addMarker(lat, long, msg, significance){
-    console.log(msg, significance)
-    L.marker([lat, long]).addTo(map).bindPopup(msg, significance)
-    return msg, significance
+// create a function to add markers
+function addMarker(lat,lng,title,message){
+    console.log(message)
+    L.marker([lat,lng]).addTo(map).bindPopup(`<h2>${title}</h2>`)
+    createButtons(lat,lng,title); // new line!!!
+    return message
 }
 
-addMarker(34.0709, -118.444, "Powell:", 'Where I work on campus')
+// create a function to add buttons with a fly to command
+function createButtons(lat,lng,title){
+    const newButton = document.createElement("button"); // adds a new button
+    newButton.id = "button"+title; // gives the button a unique id
+    newButton.innerHTML = title; // gives the button a title
+    newButton.setAttribute("lat",lat); // sets the latitude 
+    newButton.setAttribute("lng",lng); // sets the longitude 
+
+    // attach an event listner to the button with Leaflet's map.flyTo
+    newButton.addEventListener('click', function(){
+        map.flyTo([lat,lng]); 
+    })
+    document.body.appendChild(newButton); //this adds the button to our page.
+}
+
+// use our marker functions
+addMarker(40.73602157283994, -73.99738555094657,'Washington Square Park','Manhattan, New York')
+addMarker(36.11329640577107, -112.11268368742553,'Grand Canyon','National Park in Arizona')
+addMarker(34.129631348789204, -118.11452419999999,'Huntington Library','Library and Botanical Gardens in Pasadena,CA')
+addMarker(37.77705092430431, -122.43372246557624,'Alamo Square Park','San Francisco, CA')
