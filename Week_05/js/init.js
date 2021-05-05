9f1ace77a124a1debe5835760876ab4bf8ff43c2
alize <81606646+alizezila@users.noreply.github.com>
2021-05-05 12:14:54 -0700

    commit

--- Week_05/js/init.js
+++ Week_05/js/init.js
@@ -4,33 +4,37 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-let url = "https://spreadsheets.google.com/feeds/list/1PyklxDCCB1SKpSUY4cZCEEnYed9ebBvRuXWGGJaZT7E/null/public/values?alt=json"
+function addMarker(data){
+        // console.log(data)
+        // these are the names of our fields in the google sheets:
+        L.marker([data.latitude,data.longitude]).addTo(map).bindPopup(`<h2>${data.timestamp}</h2>`)
+        return data.timestamp
+}
+
+let url = "https://spreadsheets.google.com/feeds/list/1PyklxDCCB1SKpSUY4cZCEEnYed9ebBvRuXWGGJaZT7E/o57kdav/public/values?alt=json"
 fetch(url)
 	.then(response => {
 		return response.json();
 		})
-        .then(data =>{
-            // console.log(data)
-            processData(data)
+    .then(data =>{
+                // console.log(data)
+                formatData(data)
         }
-        )
+)
 
-    function processData(theData){
+
+function formatData(theData){
         const formattedData = [] /* this array will eventually be populated with the contents of the spreadsheet's rows */
-        const rows = theData.feed.entry // this is the weird Google Sheet API format we will be removing
-        // we start a for..of.. loop here 
-        for(const row of rows) { 
+        const rows = theData.feed.entry
+        for(const row of rows) {
           const formattedRow = {}
           for(const key in row) {
-            // time to get rid of the weird gsx$ format...
             if(key.startsWith("gsx$")) {
                   formattedRow[key.replace("gsx$", "")] = row[key].$t
             }
           }
-          // add the clean data
           formattedData.push(formattedRow)
         }
-        // lets see what the data looks like when its clean!
         console.log(formattedData)
-        // we can actually add functions here too
+        formattedData.forEach(addMarker)        
 }
