24663b5f5b1ee0b142f3b1f05e03dedb9511ec7b
alize <81606646+alizezila@users.noreply.github.com>
2021-05-05 11:53:58 -0700

    yhu

--- Week_05/js/init.js
+++ Week_05/js/init.js
@@ -4,11 +4,11 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-let url = "GIVE_ME_A_URL"
+let url = "https://spreadsheets.google.com/feeds/list/1PyklxDCCB1SKpSUY4cZCEEnYed9ebBvRuXWGGJaZT7E/null/public/values?alt=json"
 fetch(url)
 	.then(response => {
 		return response.json();
 		})
     .then(data =>{
         console.log(data)
-    })
+    }
