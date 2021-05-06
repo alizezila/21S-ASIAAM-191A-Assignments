b30c465952cc033b8c3c66519175d4ac101ebd2d
alize <81606646+alizezila@users.noreply.github.com>
2021-05-06 14:22:00 -0700

    ocmmitt

--- Week_06/js/init.js
+++ Week_06/js/init.js
@@ -6,26 +6,10 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 function addMarker(data){
         // console.log(data)
-        // these are the names of our lat/long fields in the google sheets:
-        L.marker([data.lat,data.lng]).addTo(map).bindPopup(`<h2>${data.wherewasthebestplaceyouvebeencampinghadoutdooradventures}</h2>${"Enjoyed it? " + data.didyoulikeit} <p>${"Your Story: " + data.optionalshareastoryaboutyourtripifyoudlike}</p>`)
-        // adding our create button function
-        createButtons(data.lat,data.lng,data.location)
+        // these are the names of our fields in the google sheets:
+        L.marker([data.lat,data.long]).addTo(map).bindPopup(`<h2>${data.wherewasthebestplaceyouvebeencampinghadoutdooradventures}</h2>${"Enjoyed it? " + data.didyoulikeit} <p>${"Your Story: " + data.optionalshareastoryaboutyourtripifyoudlike}</p>`)
         return data.timestamp
 }
-
-function createButtons(lat,lng,title){
-    const newButton = document.createElement("button"); // adds a new button
-    newButton.id = "button"+title; // gives the button a unique id
-    newButton.innerHTML = title; // gives the button a title
-    newButton.setAttribute("lat",lat); // sets the latitude 
-    newButton.setAttribute("lng",lng); // sets the longitude 
-    newButton.addEventListener('click', function(){
-        map.flyTo([lat,lng]); //this is the flyTo from Leaflet
-    })
-    const spaceForButtons = document.getElementById('contents')
-    spaceForButtons.appendChild(newButton);//this adds the button to our page.
-}
-
 
 
 let url = "https://spreadsheets.google.com/feeds/list/1PyklxDCCB1SKpSUY4cZCEEnYed9ebBvRuXWGGJaZT7E/o57kdav/public/values?alt=json"
