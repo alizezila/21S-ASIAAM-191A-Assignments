1ee7139161c700f98c72ac14bd4afabc1242579f
alize <81606646+alizezila@users.noreply.github.com>
2021-05-11 18:08:57 -0700

    map why dont u work

--- Week_06/js/init.js
+++ Week_06/js/init.js
@@ -1,16 +1,32 @@
-const map = L.map('map').setView([34.0709, -118.444], 5);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+const map = L.map('map').setView([34.0709, -118.444], 9
+        );
+    
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
 
 function addMarker(data){
         // console.log(data)
-        // these are the names of our fields in the google sheets:
-        L.marker([data.lat,data.long]).addTo(map).bindPopup(`<h2>${data.wherewasthebestplaceyouvebeencampinghadoutdooradventures}</h2>${"Enjoyed it? " + data.didyoulikeit} <p>${"Your Story: " + data.optionalshareastoryaboutyourtripifyoudlike}</p>`)
-        return data.timestamp
+        // these are the names of our lat/long fields in the google sheets:
+        L.marker([data.lat,data.long]).addTo(map).bindPopup(`<h2>${data.wherewasthebestplaceyouvebeencampinghadoutdooradventures}</h2>`)
+        // adding our create button function
+        createButtons(data.lat,data.long,data.wherewasthebestplaceyouvebeencampinghadoutdooradventures)
+        return data.wherewasthebestplaceyouvebeencampinghadoutdooradventures
 }
 
+function createButtons(lat,long,title){
+        const newButton = document.createElement("button"); // adds a new button
+        newButton.id = "button"+title; // gives the button a unique id
+        newButton.innerHTML = title; // gives the button a title
+        newButton.setAttribute("lat",lat); // sets the latitude 
+        newButton.setAttribute("lng",long); // sets the longitude 
+        newButton.addEventListener('click', function(){
+            map.flyTo([lat,long],14); //this is the flyTo from Leaflet
+        })
+        const spaceForButtons = document.getElementById("contents")
+        spaceForButtons.appendChild(newButton);//this adds the button to our page.
+}
+ 
 
 let url = "https://spreadsheets.google.com/feeds/list/1PyklxDCCB1SKpSUY4cZCEEnYed9ebBvRuXWGGJaZT7E/o57kdav/public/values?alt=json"
 fetch(url)
@@ -22,6 +38,9 @@ fetch(url)
                 formatData(data)
         }
 )
+
+
+
 
 
 function formatData(theData){
