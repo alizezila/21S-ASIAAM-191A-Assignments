ad826f75cbee2baab07cfeb0e162a2b66d3251db
alize <81606646+alizezila@users.noreply.github.com>
2021-05-14 14:46:29 -0700

    map

--- Week_07/js/init.js
+++ Week_07/js/init.js
@@ -1,21 +1,55 @@
-const map = L.map('map').setView([36.737593086418656, -119.7856438707979], 6
-        );
-    
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+const map = L.map('map').setView([36.73724014548915, -119.78847179822944], 6);
 
-function addMarker(data){
-        // console.log(data)
-        // these are the names of our lat/long fields in the google sheets:
-        L.marker([data.lat,data.long]).addTo(map).bindPopup(`<h2>${data.wherewasthebestplaceyouvebeencampinghadoutdooradventures}</h2>${"Enjoyed it? " + data.didyoulikeit} <p>${"Your Story: " + data.optionalshareastoryaboutyourtripifyoudlike}</p>`)
-       // L.circle([data.lat,data.long],{ color: 'orange', fillColor: '#f03', fillOpacity: 0.5,  radius: 400 }).addTo(map).bindPopup(`<h2>${data.bestdish}</h2>`+`<p><b>Location:</b>${data.whereisitat}</p>`+`<p><b>Name or Description:</b>${data.name}</p>`+`<p><b>How did you find it:</b>${data.yourstory}</p>`)
-     
-       // adding our create button function
-        createButtons(data.lat,data.long,data.wherewasthebestplaceyouvebeencampinghadoutdooradventures)
-        return data.timestamp
+const url = "https://spreadsheets.google.com/feeds/list/1PyklxDCCB1SKpSUY4cZCEEnYed9ebBvRuXWGGJaZT7E/o57kdav/public/values?alt=json"
+
+let Stadia_OSMBright = L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
+	maxZoom: 20,
+	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+});
+Stadia_OSMBright.addTo(map)
+
+fetch(url)
+	.then(response => {
+		return response.json();
+		})
+    .then(data =>{
+                // console.log(data)
+                formatData(data)
+        }
+)
+
+let Likedit = L.featureGroup();
+let Didntlikeit = L.featureGroup();
+let Meh = L.featureGroup();
+
+let circleOptions = {
+    radius: 6,
+    fillColor: "#ff7800",
+    color: "#000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.8
 }
 
+function addMarker(data){
+        if(data.didyoulikeit == "Yes 😎"){
+            circleOptions.fillColor = "green"
+            Likedit.addLayer(L.circleMarker([data.lat,data.long],circleOptions).bindPopup(`<h2>${data.wherewasthebestplaceyouvebeencampinghadoutdooradventures}</h2>${"Enjoyed it? " + data.didyoulikeit}<p>${"Your Story: " + data.shareastoryaboutyourtrip}</p>`))
+            createButtons(data.lat,data.long,data.wherewasthebestplaceyouvebeencampinghadoutdooradventures)
+        }
+        else if(data.didyoulikeit == "Meh.. 🥴"){
+                circleOptions.fillColor = "Yellow"
+                Meh.addLayer(L.circleMarker([data.lat,data.long],circleOptions).bindPopup(`<h2>${data.wherewasthebestplaceyouvebeencampinghadoutdooradventures}</h2>${"Enjoyed it? " + data.didyoulikeit}<p>${"Your Story: " + data.shareastoryaboutyourtrip}</p>`))
+                createButtons(data.lat,data.long,data.wherewasthebestplaceyouvebeencampinghadoutdooradventures)
+            }
+        else{
+            circleOptions.fillColor = "red"
+            Didntlikeit.addLayer(L.circleMarker([data.lat,data.long],circleOptions).bindPopup(`<h2>${data.wherewasthebestplaceyouvebeencampinghadoutdooradventures}</h2>${"Enjoyed it? " + data.didyoulikeit}<p>${"Your Story: " + data.shareastoryaboutyourtrip}</p>`))
+            createButtons(data.lat,data.long,data.wherewasthebestplaceyouvebeencampinghadoutdooradventures)
+        }
+        return data.timestamp
+    }
+    
 function createButtons(lat,long,title){
         const newButton = document.createElement("button"); // adds a new button
         newButton.id = "button"+title; // gives the button a unique id
@@ -28,21 +62,9 @@ function createButtons(lat,long,title){
         const spaceForButtons = document.getElementById("contents")
         spaceForButtons.appendChild(newButton);//this adds the button to our page.
 }
-
-let url = "https://spreadsheets.google.com/feeds/list/1PyklxDCCB1SKpSUY4cZCEEnYed9ebBvRuXWGGJaZT7E/o57kdav/public/values?alt=json"
-fetch(url)
-	.then(response => {
-		return response.json();
-		})
-    .then(data =>{
-                // console.log(data)
-                formatData(data)
-        }
-)
-
-
+ 
 function formatData(theData){
-        const formattedData = [] /* this array will eventually be populated with the contents of the spreadsheet's rows */
+        const formattedData = []
         const rows = theData.feed.entry
         for(const row of rows) {
           const formattedRow = {}
@@ -54,5 +76,17 @@ function formatData(theData){
           formattedData.push(formattedRow)
         }
         console.log(formattedData)
-        formattedData.forEach(addMarker)        
+        formattedData.forEach(addMarker)
+        Likedit.addTo(map)
+        Didntlikeit.addTo(map)
+        let allLayers = L.featureGroup([Likedit,Didntlikeit,Meh]);
+        map.fitBounds(allLayers.getBounds());        
 }
+//Leaflet feature group layers//
+let layers = {
+	"Loved it!": Likedit,
+	"Not my cup of tea": Didntlikeit,
+        "Mixed feelings...": Meh
+}
+L.control.layers(null,layers).addTo(map)
+
